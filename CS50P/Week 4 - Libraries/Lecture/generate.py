77280261d8764ad825f14189c29711d166711bb5
Xandi <santosal2018@gmail.com>
2024-11-04 21:33:03 -0500

    week 4 lecture part 1

---
+++ CS50P/Week 4 - Libraries/Lecture/generate.py
@@ -0,0 +1,21 @@
+# You don't need to import the whole random library
+# You can just import specific functions
+
+# from random import choice
+import random
+
+print('-------------------------------------------------------------')
+print()
+
+# Randomly picks an item from the list
+coin = random.choice(["heads", "tails"])
+
+print(coin)
+
+number = random.randint(1 , 10)
+
+print(number)
+
+
+print()
+print('-------------------------------------------------------------')
