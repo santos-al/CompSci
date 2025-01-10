257d563fbae317896bba326d56726d6dd086b2c7
Xandi <santosal2018@gmail.com>
2025-01-10 18:16:08 -0500

    Correct mistakes in day 3 exam

--- HackerRank/Python Challenges/1 Week Challenge/Day 3/exam.py
+++ HackerRank/Python Challenges/1 Week Challenge/Day 3/exam.py
@@ -20,13 +20,31 @@ def palindromeIndex(s):
             if new_s == new_s[::-1]: 
 
                 return count
-            else:
-
-                s = new_s[:count] + letter + new_s[count:]
-
             count += 1
 
 
 print(palindromeIndex("aaa"))
 print(palindromeIndex("aab"))
 print(palindromeIndex("baa"))
+
+def palindromeIndexCorrect(s):
+    # Two pointers to compare characters from both ends
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            # Check if removing the left character makes it a palindrome
+            if s[left + 1:right + 1] == s[left + 1:right + 1][::-1]:
+                return left
+            # Check if removing the right character makes it a palindrome
+            elif s[left:right] == s[left:right][::-1]:
+                return right
+            # If neither removal works, return -1 (shouldn't happen per problem constraints)
+            else:
+                return -1
+        left += 1
+        right -= 1
+
+    # If no mismatch is found, the string is already a palindrome
+    return -1
