2ed29f0f2c35056c29d687efd77b842fa0912ec5
Xandi <santosal2018@gmail.com>
2025-01-16 10:33:03 -0500

    time_conversion challenge

---
+++ HackerRank/Python Challenges/1 Month Challenge/Week 1/time_conversion.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'timeConversion' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def timeConversion(s):
+    # Write your code here
+    time = s[:-2]
+    meridiem = s[-2:]
+    
+    hour, minute, second = time.split(":")
+    hour = int(hour)
+    
+    if meridiem == "AM" and hour == 12:
+        hour -= 12
+    if meridiem == "PM" and hour != 12:
+        hour += 12
+    
+    form_hour = str(hour).zfill(2)
+        
+    return f"{form_hour}:{minute}:{second}"
+
+
+def timeConversionOptimized(s):
+    hour, minute, second = s[:-2].split(":")
+    meridiem = s[-2:]
+    
+    hour = int(hour)
+    if meridiem == "AM" and hour == 12:
+        hour = 0
+    elif meridiem == "PM" and hour != 12:
+        hour += 12
+    
+    return f"{str(hour).zfill(2)}:{minute}:{second}"
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
